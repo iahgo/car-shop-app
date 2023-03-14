d191c55339dff23bd7cbcb8d9f941694589a4759
iahgo <iahgo@icloud.com>
2023-03-14 16:10:56 -0300

    cars crud fix erros

--- src/routes/motorcycles.routes.ts
+++ src/routes/motorcycles.routes.ts
@@ -1,26 +1,26 @@
-import { Router } from 'express';
-import MotorcycleController from '../Controllers/motorcycle.controller';
+// import { Router } from 'express';
+// import MotorcycleController from '../Controllers/motorcycle.controller';
 
-const motorcycleRoutes = Router();
+// const motorcycleRoutes = Router();
 
-motorcycleRoutes.post(
-  '/motorcycles',
-  (req, res, next) => new MotorcycleController(req, res, next).create(),
-);
+// motorcycleRoutes.post(
+//   '/motorcycles',
+//   (req, res, next) => new MotorcycleController(req, res, next).create(),
+// );
 
-motorcycleRoutes.get(
-  '/motorcycles',
-  (req, res, next) => new MotorcycleController(req, res, next).findAll(),
-);
+// motorcycleRoutes.get(
+//   '/motorcycles',
+//   (req, res, next) => new MotorcycleController(req, res, next).findAll(),
+// );
 
-motorcycleRoutes.get(
-  '/motorcycles/:id',
-  (req, res, next) => new MotorcycleController(req, res, next).findById(),
-);
+// motorcycleRoutes.get(
+//   '/motorcycles/:id',
+//   (req, res, next) => new MotorcycleController(req, res, next).findById(),
+// );
 
-motorcycleRoutes.put(
-  '/motorcycles/:id',
-  (req, res, next) => new MotorcycleController(req, res, next).updateMotorcycle(),
-);
+// motorcycleRoutes.put(
+//   '/motorcycles/:id',
+//   (req, res, next) => new MotorcycleController(req, res, next).updateMotorcycle(),
+// );
 
-export default motorcycleRoutes;
+// export default motorcycleRoutes;
